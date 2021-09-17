cfdd0c04f240d800949427dd102fef5a6e10151b
Alejandro Sánchez Peralta <65984679+sanalexito@users.noreply.github.com>
2021-09-17 17:41:52 -0500

    Update ubicaciones_conalep.R

--- ubicaciones_conalep.R
+++ ubicaciones_conalep.R
@@ -7,28 +7,42 @@ data <- data %>% filter(!data$latitud%in%"(null)" & !data$latitud%in%0) %>%
          mutate(Latitud = as.numeric(latitud),
                 Longitud= as.numeric(longitud))
 
-Latitud <- as.numeric(data$latitud)
-Longitud<- as.numeric(data$longitud)
 
-data$popup <- paste("<b>Plantel #: </b>", data$clave_plantel ,"<br>", "<b>CCT: </b>", data$cct,
-                    "<br>", "<b>Description: </b>", data$plantel,
-                    "<br>", "<b>Entidad: </b>", data$clave_entidad,
-                    "<br>", "<b>Domicilio: </b>", data$domicilio_plantel,
-                    "<br>", "<b>Longitud: </b>", data$longitud,
-                    "<br>", "<b>Latitud: </b>", data$latitud)
 
-leaflet(data) %>% addTiles() %>%
-  addTiles(group = "OSM (default)") %>%
-  addProviderTiles(provider = "Esri.WorldStreetMap", group = "World StreetMap") %>%
-  addProviderTiles(provider = "Esri.WorldImagery", group = "World Imagery") %>%
-  # addProviderTiles(provider = "NASAGIBS.ViirsEarthAtNight2012",group = "Nighttime Imagery") %>%
-  addMarkers(lng = Longitud[1:120], 
-             lat = Latitud[ 1:120], 
-             popup = data$popup, 
-             clusterOptions = markerClusterOptions()) %>%
-  addLayersControl(
-    baseGroups = c("OSM (default)","World StreetMap", "World Imagery"),
-    options = layersControlOptions(collapsed = T)
-  )
+bla <- list()
+mapa <-list()
+cve_ent <- 1:32
+for(i in cve_ent){
+  bla[[i]] <- data[data$clave_entidad%in%cve_ent[i],]
+  
+  
+  bla[[i]]$popup <- paste("<b>Plantel #: </b>", bla[[i]]$clave_plantel ,"<br>", "<b>CCT: </b>", bla[[i]]$cct,
+                     "<br>", "<b>Description: </b>", bla[[i]]$plantel,
+                     "<br>", "<b>Entidad: </b>", bla[[i]]$entidad,
+                     "<br>", "<b>Domicilio: </b>", bla[[i]]$domicilio_plantel,
+                     "<br>", "<b>Longitud: </b>", bla[[i]]$longitud,
+                     "<br>", "<b>Latitud: </b>", bla[[i]]$latitud)
+  Latitud <- as.numeric(bla[[i]]$latitud)
+  Longitud<- as.numeric(bla[[i]]$longitud)
+  
+ mapa[[i]]<- bla[[i]] %>% leaflet() %>% addTiles() %>%
+    addTiles(group = "OSM (default)") %>%
+    addProviderTiles(provider = "Esri.WorldStreetMap", group = "World StreetMap") %>%
+    addProviderTiles(provider = "Esri.WorldImagery", group = "World Imagery") %>%
+    #addProviderTiles(provider = "NASAGIBS.ViirsEarthAtNight2012",group = "Nighttime Imagery") %>%
+    addMarkers(lat = Latitud, 
+               lng = Longitud, 
+               popup = bla[[i]]$popup,
+               clusterOptions = markerClusterOptions()) %>%
+    addLayersControl(
+      baseGroups = c("OSM (default)","World StreetMap", "World Imagery"),
+      options = layersControlOptions(collapsed = T)
+    ) 
+  
+}
+
+#Para visualizar los planteles por cada entidad se despliega cada uno de los mapas-
+View(mapa[[1]])
+
 
 
